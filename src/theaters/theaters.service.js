9a70953b6cd69c313c584efc66c64b86bd5d703a
Shirene Boyd <shirenekadkhodai@gmail.com>
2021-12-07 17:16:04 -0600

    Edited comments

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,6 +1,9 @@
 const knex = require("../db/connection");
 const reduceProperties = require("../utils/reduce-properties");
 
+//Reduces an array of data by mapping properties onto array properties as objects.
+    //each key is the source property and the value is an array representing the path to the new property.
+        //Since array index values are not known at configuration time, use `null` to represent unknown index values.
 const reducedMovies = reduceProperties("theater_id", {
         movie_id: ["movies", null, "movie_id"],
         title: ["movies", null, "title"],
