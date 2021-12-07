dd9fe1699b1ee3ab6ff4d6a034ee19e621657c83
Shirene Boyd <shirenekadkhodai@gmail.com>
2021-12-07 17:09:51 -0600

    Formatted code

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,65 +1,47 @@
-//const { first } = require("../db/connection");
 const knex = require("../db/connection");
 const mapProperties = require("../utils/map-properties");
-const reduceProperties = require("../utils/reduce-properties");
 
 const addCritic = mapProperties({
-    preferred_name: "critic.preferred_name",
-    surname: "critic.surname",
-    organization_name: "critic.organization_name",
-})
-
-const reducedMovies = reduceProperties("theater_id", {
-    movie_id: ["movies", null, "movie_id"],
-    title: ["movies", null, "title"],
-    runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
-    rating: ["movies", null, "rating"],
-    description: ["movies", null, "description"],
-    image_url: ["movies", null, "image_url"],
-    is_showing: ["movies", null, "is_showing"],
+  preferred_name: "critic.preferred_name",
+  surname: "critic.surname",
+  organization_name: "critic.organization_name",
 });
 
-function create(review){
-    return knex("reviews")
+function create(review) {
+  return knex("reviews")
     .insert(review)
     .returning("*")
     .then((createdRecords) => createdRecords[0]);
 }
 
 function read(review_id) {
-    return knex("reviews")
-    .select("*")
-    .where({ review_id })
-    .first();
+  return knex("reviews").select("*").where({ review_id }).first();
 }
 
 function update(updatedReview) {
-    return knex("reviews")
+  return knex("reviews")
     .select("*")
-    .where({ review_id:updatedReview.review_id })
+    .where({ review_id: updatedReview.review_id })
     .update(updatedReview, "*");
 }
 
-
 function newlyUpdatedReview(reviewId) {
-    return knex("reviews as r")
+  return knex("reviews as r")
     .join("critics as c", "c.critic_id", "r.critic_id")
     .select("*")
-    .where({ "r.review_id":reviewId })
+    .where({ "r.review_id": reviewId })
     .first()
-    .then(addCritic)
-    //.then(reducedMovies);
+    .then(addCritic);
 }
 
 function destroy(review_id) {
-    return knex("reviews").where({ review_id }).del();
+  return knex("reviews").where({ review_id }).del();
 }
-
 
 module.exports = {
-    create,
-    read,
-    update,
-    delete: destroy,
-    newlyUpdatedReview,
-}
+  create,
+  read,
+  update,
+  delete: destroy,
+  newlyUpdatedReview,
+};
